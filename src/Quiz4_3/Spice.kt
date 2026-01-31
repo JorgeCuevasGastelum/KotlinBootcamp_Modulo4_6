17765c5e0709417ed43d9e4ead62f67469c01a61
JorgeCuevasGastelum <jorge.cuevas252274@potros.itson.edu.mx>
2026-01-30 23:12:29 -0700

    4.5 Quiz: Practice Time (Objetos)

---
+++ src/Quiz4_3/Spice.kt
@@ -0,0 +1,47 @@
+package Quiz4_3
+//clase personalizada
+class Spice(
+    val name: String,
+    val spiciness: String = "mild"
+) {
+
+    val heat: Int
+        get() = when (spiciness) {
+            "mild" -> 5
+            "medium" -> 7
+            "spicy" -> 10
+            "very spicy" -> 15
+            else -> 0
+        }
+    //Constructor
+    init {
+        println("Spice created: $name, spiciness: $spiciness, heat: $heat")
+    }
+
+}
+
+//funcion que crea objeto de la clase
+fun makeSalt() = Spice("salt", "not spicy")
+
+fun main() {
+
+    val spices = listOf(
+        Spice("curry", "mild"),
+        Spice("pepper", "medium"),
+        Spice("cayenne", "spicy"),
+        Spice("ginger", "mild"),
+        Spice("red curry", "very spicy"),
+        Spice("green curry", "medium")
+    )
+    //filtrar los spices segun su heat
+    val mildSpices = spices.filter { it.heat <= 5 }
+
+    //imprimir los resultados de el filtro
+    println("\nMild or less spicy spices:")
+    mildSpices.forEach { println(it.name) }
+
+    //creacion de objeto
+    val salt = makeSalt()
+}
+
+
